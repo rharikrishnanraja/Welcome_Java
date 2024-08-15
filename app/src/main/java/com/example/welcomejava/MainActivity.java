56cb1217d2964eff368747854ab490e745a2bd82
HP <harikrishnan290604@outlook.com>
2024-08-15 09:55:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/welcomejava/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.welcomejava;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                }
+        );
+
+    }
+}
